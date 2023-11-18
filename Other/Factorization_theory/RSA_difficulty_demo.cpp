20aab2e52441b6535b0a12c9cf1da213e9848a2e
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2023-11-17 17:44:20 -0700

    Add files via upload

---
+++ Other/Factorization_theory/RSA_difficulty_demo.cpp
@@ -0,0 +1,88 @@
+/* Exposing that it seems impossible to know any  semiprime factor digit  unless
+the whole factor is known. This tool prints all possible ways two numbers can be
+multiplied in order to recreate some last few digits of your semiprime. It's not
+possible to eliminate any factorization possibilities  because the multiplicands
+and multipliers listed are simply all numbers ending in 1, 3, 7, 9. For example,
+given the last few digits of semiprime pq,  primes p are listed as every integer
+1+ but ending in 1, 3, 7, & 9  (as primes should, other than 2 & 5.) Now, for q,
+the same numbers are listed verbatim as multipliers for p, in a different order.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+It seems RSA has the perfect secrecy-like nature of the  One-time pad's  all-way
+function. As if RSA starts at a One-time pad then takes steps down from there.*/
+
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{	//                               user knobs
+	
+	/*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//////////////////////////////////////
+	\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  /////////////////////////////////////
+	\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\    ////////////////////////////////////
+	\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\      ///////////////////////////////////
+	\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\        //////////////////////////////////
+	\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\            ////////////////////////////////
+	\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\              ///////////////////////////////
+	\\\\\\\\\\\\\\\\\\\\\\\\\\\\\                  /////////////////////////////
+	\\\\\\\\\\\\\\\\\\\\\\\\\\\                      ///////////////////////////
+	\\\\\\\\\\\\\\\\\\\\\\\                              ///////////////////////
+	\\\\\\\\\\\\\\\\\\                                        ////////////////*/
+	
+	long long   last_few_digits_of_your_semiprime = 423446789015381247; //Must end in 1, 3, 7, 9
+	long long    number_of_ending_digits_to_crack =                  1; //Range: 1+     <------------------------Increase him! Slowly!
+	
+	/*////////////////                                        \\\\\\\\\\\\\\\\\\
+	///////////////////////                              \\\\\\\\\\\\\\\\\\\\\\\
+	///////////////////////////                      \\\\\\\\\\\\\\\\\\\\\\\\\\\
+	/////////////////////////////                  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	///////////////////////////////              \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	////////////////////////////////            \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	//////////////////////////////////        \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	///////////////////////////////////      \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	////////////////////////////////////    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	/////////////////////////////////////  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	//////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+	
+	long long number_of_ending_digits_via_mod = 1;
+	for(long long a = 0; a < number_of_ending_digits_to_crack; a++) {number_of_ending_digits_via_mod *= 10;}
+	last_few_digits_of_your_semiprime %= number_of_ending_digits_via_mod;
+	
+	long long p_duplicate_watch = 0;
+	long long q_duplicate_watch = 0;
+	
+	long long quantity = 1;
+	for(long long p = 0; p < number_of_ending_digits_via_mod; p++)
+	{	for(long long q = 0; q < number_of_ending_digits_via_mod; q++)
+		{	if(((p % 10) == 1)
+			|| ((p % 10) == 3)
+			|| ((p % 10) == 7)
+			|| ((p % 10) == 9))
+			{	if(((q % 10) == 1)
+				|| ((q % 10) == 3)
+				|| ((q % 10) == 7)
+				|| ((q % 10) == 9))
+				{	long long product = (p * q);
+					product %= number_of_ending_digits_via_mod;
+					
+					if(product == last_few_digits_of_your_semiprime)
+					{	cout << "Factorization possibility " << quantity << ":   " << p << " * " << q << " mod " << number_of_ending_digits_via_mod << " = " << product << "\n";
+						
+						p_duplicate_watch += p;
+						q_duplicate_watch += q;
+						quantity++;
+					}
+				}
+			}
+		}
+	}
+	
+	if(p_duplicate_watch != q_duplicate_watch) {cout << "\n\nFAILED! Cryptographic semiprimes end in 1, 3, 7, or 9."; return 0;}
+	
+	cout << "\n\n\nQuantity is always 4*10^n because 40% of numbers end in 1, 3, 7, 9."
+	     << "\n\n~Half are duplicates, as in a*b and b*a."
+	     << "\n\nInterestingly, sums are always 2*10^n and always equal:"
+	     << "\nsum(all p) = " << p_duplicate_watch 
+	     << "\nsum(all q) = " << q_duplicate_watch;
+}
